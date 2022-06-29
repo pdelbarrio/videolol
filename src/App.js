d90c6baa23fc4d8ccda4266826a3ac7e4ed23eb3
Pablo del Barrio <katatonio@hotmail.com>
2022-06-29 12:21:49 +0200

    Added some routes and firebase dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
+import SignIn from "./routes/sign-in/sign-in";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
     </Routes>
   );
 };

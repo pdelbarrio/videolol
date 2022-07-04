93c006079ac45d61f244932ed0f8d778a5d1a2f7
Pablo del Barrio <katatonio@hotmail.com>
2022-07-04 18:16:24 +0200

    created authentication page component alongside sign in form

---
+++ src/routes/authentication/authentication.jsx
@@ -0,0 +1,15 @@
+import SignInForm from "../../components/sign-in-form/sign-in-form";
+import SignUpForm from "../../components/sign-up-form/sign-up-form";
+import "./authentication.scss";
+
+const Authentication = () => {
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Authentication;

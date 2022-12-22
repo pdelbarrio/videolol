cd4a0295e3f7e305b8ae2a33363127e931bd4165
Pablo del Barrio <pablodbb@gmail.com>
2022-12-22 18:55:32 +0100

    Added stripe functionality

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import {
   selectCartTotal,
 } from "../../store/cart/cart.selector";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import {
   CheckoutContainer,
@@ -40,6 +41,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: {cartTotal}€</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };

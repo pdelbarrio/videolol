d5b9e8a43acfce463a67db6bea20ec79a71f8ec0
Pablo del Barrio <pablodbb@gmail.com>
2023-01-10 11:59:47 +0100

    Added lazy loading in main components of the routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component";
-import Home from "./routes/home/home.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
-import { useEffect } from "react";
+import Spinner from "./components/spinner/spinner.component";
+import { useEffect, lazy, Suspense } from "react";
 import { checkUserSession } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
+const Navigation = lazy(() =>
+  import("./routes/navigation/navigation.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,14 +22,16 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
